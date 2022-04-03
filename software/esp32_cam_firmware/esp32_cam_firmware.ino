ff6c4dbdea0d5639fcb0f2228e963376d1b67c56
Bo Han Zhu <bohanzhu9@gmail.com>
2022-04-03 05:07:24 -0400

    Update esp32_cam_firmware.ino

--- software/esp32_cam_firmware/esp32_cam_firmware.ino
+++ software/esp32_cam_firmware/esp32_cam_firmware.ino
@@ -49,10 +49,10 @@ void handleJpgMid(void);
  * Stepper Motor
  */
 typedef enum {
-  PLASTIC_BIN = 0,
-  PAPER_BIN   = 1,
-  CAN_BIN     = 2,
-  DEFAULT_BIN = 3,
+  RECYCLE_BIN         = 0,
+  COMPOST_BIN         = 1,
+  ELECTRONICS_BIN     = 2,
+  DEFAULT_BIN         = 3,
   
 } bin_t;
 
@@ -67,7 +67,7 @@ typedef enum {
 Stepper stepper = Stepper(NUMBER_OF_STEPS_PER_REV, STEPPER_IN1, STEPPER_IN2, STEPPER_IN3, STEPPER_IN4);
 
 static bin_t current_bin = DEFAULT_BIN;
-static volatile bin_t next_bin    = CAN_BIN;
+static volatile bin_t next_bin = ELECTRONICS_BIN;
 
 /**
  * RGB LED
@@ -76,7 +76,6 @@ typedef enum {
   LED_IDLE    = 0,
   LED_PENDING = 1,
   LED_APPROVE = 2,
-  LED_TIMEOUT = 3,
   
 } led_status_t;
 
@@ -141,7 +140,6 @@ void handle_cam_and_server(void * no_params) {
  * @param[in]: N/A -- need void pointer for FreeRTOS implementation
  */
 void control_stepper(void * no_params) {
-
   // Set speed, 10 rpm is about the limit
   stepper.setSpeed(10);
 
@@ -160,7 +158,6 @@ void control_stepper(void * no_params) {
   }
 }
 
-
 /**
  * @brief: Receives led status commands and relays to the RGB LED. Acts as a state machine based on LED status.
  * @param[in]: N/A -- need void pointer for FreeRTOS implementation
@@ -178,8 +175,8 @@ void control_rgb_led(void * no_params) {
     switch (led_status) {
       case LED_IDLE:
       {
-        // Display nothing (black)
-        rgb_write(0, 0);
+        // Display red
+        rgb_write(255, 0);
 
         // Set timer flag to true in preparation for pending state
         set_timer = true;
@@ -197,20 +194,6 @@ void control_rgb_led(void * no_params) {
 
         // Display yellow
         rgb_write(255, 255);
-        break;
-      }
-
-      case LED_TIMEOUT:
-      {
-        // Will only enter this state if the timer went off
-        // Display red
-        rgb_write(255, 0);
-
-        // Delay task for 1 second
-        vTaskDelay(pdMS_TO_TICKS(1000));
-
-        // Move to idle
-        led_status = LED_IDLE;
         break;
       }
 
@@ -237,6 +220,25 @@ void control_rgb_led(void * no_params) {
       }
     } // end switch
   } // end while
+}
+
+/**
+ * @brief: Parses commands from the serial input and copies them into the corresponding volatile variables
+ */
+void parse_serial_commands(void * no_params) {
+  String incoming_string = "";
+
+  while (true) {
+    while (!Serial.available());
+    incoming_string = Serial.readString();
+    
+    led_status = (led_status_t) incoming_string.substring(1,2).toInt();
+    if (led_status == LED_APPROVE) {
+      next_bin = (bin_t) incoming_string.substring(0, 1).toInt();
+    }
+
+    incoming_string = "";
+  }
 }
 
 /**
@@ -270,6 +272,15 @@ void setup() {
     2048,
     NULL,
     2,
+    NULL,
+    app_cpu);
+
+  xTaskCreatePinnedToCore(
+    parse_serial_commands,
+    "Parse Serial Commands",
+    1024,
+    NULL,
+    1, // Lowest priority
     NULL,
     app_cpu);
   
@@ -356,5 +367,5 @@ void rgb_write(int red, int green) {
  * @brief: ISR for detection timeout, transitions LED status into LED_TIMEOUT
  */
 void IRAM_ATTR onTimer(void){
-  led_status = LED_TIMEOUT;
+  led_status = LED_IDLE;
 }
